e778acc96076cbbd811b9892528aab063af35bd8
lujain <lojain7890ii@gmail.com>
2023-03-22 11:06:20 +0300

    add comment

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hi my name is lujain
+        //hi
         int a =0;
     }
 }
